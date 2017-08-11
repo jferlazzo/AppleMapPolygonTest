314efc5e078ad5c3d33e3cf00253aae1a44f9772
John Ferlazzo <jferlazzo@civica.com.au>
2017-08-11 12:49:21 +1000

    Created separate PCL. Renamed project and solution

--- AppleMapPolygonTest/MapViewController.cs
+++ AppleMapPolygonTest/MapViewController.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using CoreLocation;
 using MapKit;
 using UIKit;
+using XamarinMapsDrawPolygon.Portable;
 
 namespace AppleMapPolygonTest
 {
@@ -73,50 +73,5 @@ namespace AppleMapPolygonTest
 
             return polygonView;
         }
-    }
-
-    public class LocationSampleData
-    {
-        public List<GpsLocation> Locations { get; }
-
-        public LocationSampleData()
-        {
-            Locations = new List<GpsLocation>
-            {
-                new GpsLocation
-                {
-                    Title        = "Civica Pty Ltd",
-                    Latitude     = -37.816477823733315,
-                    Longitude    = 144.95702102780342,
-                    BoundaryData = new List<GpsBoundary>
-                    {
-                        new GpsBoundary {Sequence = 0, Latitude = -37.81609271483047,  Longitude = 144.95705422013998},
-                        new GpsBoundary {Sequence = 1, Latitude = -37.8161976002603,   Longitude = 144.9566887691617},
-                        new GpsBoundary {Sequence = 2, Latitude = -37.816781353818726, Longitude = 144.95701784268022},
-                        new GpsBoundary {Sequence = 3, Latitude = -37.81670110122117,  Longitude = 144.95729142799973}
-                    }
-                }
-            };
-        }
-    }
-
-    public class GpsLocation
-    {
-        public string Title { get; set; }
-
-        public double Latitude { get; set; }
-
-        public double Longitude { get; set; }
-
-        public List<GpsBoundary> BoundaryData { get; set; }
-    }
-
-    public class GpsBoundary
-    {
-        public int Sequence { get; set; }
-
-        public double Latitude { get; set; }
-
-        public double Longitude { get; set; }
-    }
+    } 
 }
